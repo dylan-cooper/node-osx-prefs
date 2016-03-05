3730ff23dfddddb78cde2c9a1501114ddd93f750
Dylan Cooper <dcoope05@mail.uoguelph.ca>
2016-03-05 15:26:36 -0500

    Used Mocha testing framework for unit tests

--- test.js
+++ test.js
@@ -1,32 +1,48 @@
 const assert = require('chai').assert;
 const nodeOsxPrefs = require ('./lib/index');
-
-var dm = nodeOsxPrefs.isDarkMode();
-assert(dm === true || dm === false);
-
-var color = nodeOsxPrefs.getAppearanceColor();
-assert(color === 'Blue' || color === 'Graphite');
-
-var color = nodeOsxPrefs.getHighlightColor();
-assert(Array.isArray(color));
-assert(color.length === 3);
-for (var i = 0; i < 3; i++){
-  //In range
-  assert(color[i] >=0);
-  assert(color[i] <= 255);
-  //Integer
-  assert(color[i] % 1 === 0);
-}
-
-var size = nodeOsxPrefs.getSidebarIconSize();
-assert(size === 'Small' || size === 'Medium' || size === 'Large');
-
-var visibility = nodeOsxPrefs.getScrollbarVisibility();
-assert(visibility == 'Automatic' || visibility == 'WhenScrolling' || visibility == 'Always');
-
-var paging = nodeOsxPrefs.isScrollbarPaging();
-assert(paging === true || paging === false);
-
-console.log('All tests passed!');
-
-process.exit();
+describe('node-osx-prefs', function(){
+  describe('isDarkMode', function(){
+    it('returns a boolean', function(){
+      var dm = nodeOsxPrefs.isDarkMode();
+      assert(dm === true || dm === false);
+    });
+  });
+  describe('getAppearanceColor', function(){
+    it('returns Blue or Graphite', function(){
+      var color = nodeOsxPrefs.getAppearanceColor();
+      assert(color === 'Blue' || color === 'Graphite');
+    });
+  });
+  describe('getHighlightColor', function(){
+    it('returns an array of 3 integers between 0 and 255', function(){
+      var color = nodeOsxPrefs.getHighlightColor();
+      assert(Array.isArray(color));
+      assert(color.length === 3);
+      for (var i = 0; i < 3; i++){
+        //In range
+        assert(color[i] >=0);
+        assert(color[i] <= 255);
+        //Integer
+        assert(color[i] % 1 === 0);
+      }
+    });
+  });
+  describe('getSidebarIconSize', function(){
+    it('returns Small, Medium or Large', function(){
+      var size = nodeOsxPrefs.getSidebarIconSize();
+      assert(size === 'Small' || size === 'Medium' || size === 'Large');
+    });
+  });
+  describe('getScrollbarVisibility', function(){
+    it('returns Automatic, WhenScrolling or Always', function(){
+      var visibility = nodeOsxPrefs.getScrollbarVisibility();
+      assert(visibility == 'Automatic' || visibility == 'WhenScrolling' || visibility == 'Always');
+    });
+  });
+  describe('isScrollbarPaging', function(){
+    it('returns a boolean', function(){
+      var paging = nodeOsxPrefs.isScrollbarPaging();
+      assert(paging === true || paging === false);
+    });
+  });
+});
